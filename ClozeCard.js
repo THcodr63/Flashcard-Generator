b8c3ffaaf7f8f4666fb9ff3e9527f55bf9d560a9
Tiara Hamilton <tnhami63@gmail.com>
2017-08-24 23:25:36 -0400

    Added

---
+++ ClozeCard.js
@@ -0,0 +1,12 @@
+function clozeCard(text, close) {
+    this.partial = text.replace(cloze, "...Moose..."),
+    this.fullText = fulltext, 
+    this.cloze = cloze
+    this.cardCloze function(err) {
+      if (err) throw (err);
+          console.log(err);
+    }
+}
+
+
+module.exports = ClozeCard;
